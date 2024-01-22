3845b435702b67a0a4d872d18a3a08c2bd37a657
Adam Stádník <adam.stadnik@seznam.cz>
2024-01-22 18:01:28 +0100

    Added homeicon

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import Link from "next/link";
+import Image from 'next/image';
 
 const Navbar = () => {
   return (
     <nav className="bg-indigo-600 w-full h-16 text-white p-5 flex justify-between">
-      <Link href="/">HomePage</Link>
+      <Link href="/"><Image src="/assets/icons/home.svg" height={25} width={25} alt="home-icon"/></Link>
       <Link href="/contact">Contact us here!</Link>
     </nav>
   );
